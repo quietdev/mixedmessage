872bd219ccc33ab01e7fbcbc2c5c9303b8f2bc09
H2O <h2odevelops@gmail.com>
2021-03-09 18:36:12 -0500

    Bug fixes and finalization

--- main.js
+++ main.js
@@ -2,22 +2,30 @@
 //First portfolio program
 
 const potentialAnswers = {
-    mood = ['sad', 'happy', 'bored', 'annoyed', 'terrified', 'anxious', 'excited'],
-    thought = ['why am I running this program?', 'where is she right now?', "is she thinking about me?"],
-    occupation = ['programmer', 'pianist', 'guitarist', 'jump roping professional', 'ice cream server'],
+    mood: ['sad', 'happy', 'bored', 'annoyed', 'terrified', 'anxious', 'excited'],
+    thought: ['why am I running this program?', 'where is she right now?', "is she thinking about me?"],
+    occupation: ['programmer', 'pianist', 'guitarist', 'jump roping professional', 'ice cream server'],
 }
 
 const randomMood = () => {
-    let x = Math.floor(Math.random() * potentialAnswers.mood.length - 1)
-    return mood[x]
+    let x = Math.floor(Math.random() * (potentialAnswers.mood.length - 1))
+    return potentialAnswers.mood[x]
 }
 
 const randomThought = () => {
-    let x = Math.floor(Math.random() * potentialAnswers.thought.length - 1)
-    return thought[x]
+    let x = Math.floor(Math.random() * (potentialAnswers.thought.length - 1))
+    return potentialAnswers.thought[x]
 }
 
 const randomOccupation = () => {
-    let x = Math.floor(Math.random() * potentialAnswers.occupation.length - 1)
-    return occupation[x]
+    let x = Math.floor(Math.random() * (potentialAnswers.occupation.length - 1))
+    return potentialAnswers.occupation[x]
 }
+
+const giveMessage = () => {
+    console.log(`Hello I can tell you're feeling ${randomMood()}.`)
+    console.log(`I bet you're asking yourself ${randomThought()}`)
+    console.log(`Why do I have a feeling you're a ${randomOccupation()}.`)
+}
+
+giveMessage();
